f3619e515a0afaeb66d098bb1086555f02baf1f8
achjiang <htxz_jiang@163.com>
2019-04-14 00:06:49 +0800

    add config

--- manage.py
+++ manage.py
@@ -7,9 +7,12 @@
 from flask import Flask
 # 导入SQLAlchemy用以创建数据库
 from flask_sqlalchemy import SQLAlchemy
+# 导入redis用于缓存
 import redis
 # 导入session
 from flask_session import Session
+# 前端的csrf防护机制
+from flask_wtf import CSRFProtect
 
 
 # I 创建app
@@ -32,10 +35,16 @@ class Config(object):
 	REDIS_HOST = "127.0.0.1"
 	REDIS_PORT = 6379
 
-	# flask-session配置
+	# flask-session配置：https://pythonhosted.org/Flask-Session/
+	# 1.配置flask_session缓存的数据类型，
+	# 可以是redis,memcached,mongodb,sqlalchemy,filesystem等
 	SESSION_TYPE = "redis"
-	# 本身redis数据库和用于缓存的redis数据库可能是两台不同的服务器，这里项目为方便使用同一台服务器
+	# 2.对应redis的配置
+	# 本身redis数据库和用于缓存的redis数据库可能是两台不同的服务器，
+	# 这里项目为方便使用同一台服务器
 	SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+	# 3.配置session数据的有效期
+	PERMANENT_SESSION_LIFETIME = 86400
 
 
 
@@ -51,7 +60,11 @@ db = SQLAlchemy(app)
 redis_store = redis.SrtictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 
 # 利用flask_session 将session数据保存到redis数据库中
-Session(app)
+Session(app)    # 把Session当作类，将app作为参数传入这个类中
+
+# V 为flask补充csrf防护机制
+CSRFProtect(app)    # 把CSRFProtect当作类，将app作为参数传入这个类中
+
 
 #
 @app.route("/index")
