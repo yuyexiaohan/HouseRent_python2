985d7d68ae9ea39554b68f2de4f87e05b3b7d9e1
achjiang <htxz_jiang@163.com>
2019-04-15 15:54:19 +0800

    add response_code.py

--- ihome/api_1_0/verify_code.py
+++ ihome/api_1_0/verify_code.py
@@ -6,6 +6,8 @@
 from . import api
 from ihome.utils.captcha.captcha import captcha
 from ihome import redis_store
+from ihome import constants
+from flask import current_app, jsonify # ?
 
 
 # restful风格的url:GET 127.0.0.1/api/v1.0/image_codes/<image_code_id>
@@ -13,7 +15,7 @@ from ihome import redis_store
 def get_image_code(image_code_id):
 	"""
 	获取图片验证码
-	:return: 验证码图片
+	:return: 正常情况：验证码图片 异常：返回json
 	"""
 	# 业务逻辑处理
 
@@ -26,10 +28,18 @@ def get_image_code(image_code_id):
 	# image_codes: {"":"", "":"", "":"",}  hash数据类型 hset("image_codes", "id1", "abc")
 
 	# 单条维护记录
-	redis_store.set("image_code_%s" % image_code_id, text)
-	# 设置有效期
-	redis_store.expire("image_code_%s" % image_code_id, 180)
+	# 使用set和expire方法进行组合使用，配置存储及过去时间
+	# redis_store.set("image_code_%s" % image_code_id, text)
+	# # 设置有效期
+	# redis_store.expire("image_code_%s" % image_code_id, constants.IMAGE_CODE_REDIS_EXPIRES)
 
+	# 直接使用setex方法对文本和有效期进行设置
+	# redis_store.setex(mage_code_id, 有效期, text)
+	try:
+		redis_store.setex("image_code_%s" % image_code_id, constants.IMAGE_CODE_REDIS_EXPIRES, text)
+	except Exception as e:
+		current_app.logger.error("redis存储图像识别码错误:%s" % e)
+		return jsonify(errno=1, error)
 	# 返回图片
 
 
--- ihome/constants.py
+++ ihome/constants.py
@@ -3,3 +3,6 @@
 # Author : achjiang
 
 # 常量文件汇总
+
+# 图像验证码在redis中的有效期， 单位：秒
+IMAGE_CODE_REDIS_EXPIRES = 180
