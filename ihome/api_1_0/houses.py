fa2b1b6e26062c72f31c63b063863d28c490136f
achjiang <htxz_jiang@163.com>
2019-04-20 00:41:05 +0800

    add house view file

--- ihome/api_1_0/houses.py
+++ ihome/api_1_0/houses.py
@@ -4,3 +4,23 @@
 # Filename : houses
 
 
+from . import api
+from ihome.utils.commons import login_required
+from flask import g, current_app, jsonify, request, session
+from ihome.utils.response_code import RET
+from ihome.utils.image_storage import storage
+from ihome import db, constants
+from ihome.models import Area
+
+
+@api.route("/areas", methods="[GET]")
+@login_required
+def get_areas_info():
+	"""获取地域信息"""
+	# 查询数据库，获取房屋信息
+	
+	#
+	pass
+
+
+
