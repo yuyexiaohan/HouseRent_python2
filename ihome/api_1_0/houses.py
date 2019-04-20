2b4abaed36e85103e9c9872329852d713e6c2176
achjiang <htxz_jiang@163.com>
2019-04-20 23:05:48 +0800

    add get-areas-info function

--- ihome/api_1_0/houses.py
+++ ihome/api_1_0/houses.py
@@ -3,7 +3,6 @@
 # Author : achjiang
 # Filename : houses
 
-
 from . import api
 from ihome.utils.commons import login_required
 from flask import g, current_app, jsonify, request, session
@@ -13,18 +12,24 @@ from ihome import db, constants
 from ihome.models import Area
 
 
-@api.route("/areas", methods="[GET]")
-@login_required
+# @api.route("/areas", methods="[GET]")
+@api.route("/areas")
+# @login_required
 def get_areas_info():
 	"""获取地域信息"""
 	# 查询数据库，获取房屋信息
+	print("*" * 20)
 	try:
 		area_li = Area.query.all()
 	except Exception as e:
 		current_app.logger.error(e)
 		return jsonify(errno=RET.DBERR, errmsg="数据库异常")
-	#
-	pass
+
+	area_dict_li = []
+	# 将对象转换为字典
+	for area in area_li:
+		area_dict_li.append(area.to_dict()) # to_dict是返回一个字典
+	return jsonify(errno=RET.OK, errmsg="OK", data=area_dict_li)
 
 
 
