93ef2217fdc9f634b53b85f92759a68d1bb2a41c
achjiang <htxz_jiang@163.com>
2019-04-22 22:27:02 +0800

    add celery

---
+++ ihome/tasks/task_sms.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python 
+# coding=utf-8
+# @Time : 2019/4/22 0:06
+# @Author : achjiang
+# @File : task_sms.py
+
+from celery import Celery
+from ihome.libs.rlyuntongxun.send_smspy import CCP
+
+
+# 定义celery对象
+celery_app = Celery("ihome", broker="redis://127.0.0.1:6379/1")
+
+
+@celery_app.tasks
+def send_sms(to, datas, temp_id):
+    """
+    发送短信异步任务
+    :param to:
+    :param datas:
+    :param temp_id:
+    :return:
+    """
+    ccp = CCP()
+    ccp.send_template_sms()
