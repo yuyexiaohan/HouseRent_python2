29708781cf75b1affaf8cc48e42e7a15a2d5427f
achjiang <htxz_jiang@163.com>
2019-04-23 01:51:54 +0800

    add houses show param

--- ihome/constants.py
+++ ihome/constants.py
@@ -25,3 +25,21 @@ QINIU_URL_DOMAIN = "http://pq53vhclw.bkt.clouddn.com/"
 
 # 城区信息的缓存时间, 单位：秒
 AREA_INFO_REDIS_CACHE_EXPIRES = 7200
+
+# 首页展示最多的房屋数量
+HOME_PAGE_MAX_HOUSES = 5
+
+# 首页房屋数据的Redis缓存时间，单位：秒
+HOME_PAGE_DATA_REDIS_EXPIRES = 7200
+
+# 房屋详情页展示的评论最大数
+HOUSE_DETAIL_COMMENT_DISPLAY_COUNTS = 30
+
+# 房屋详情页面数据Redis缓存时间，单位：秒
+HOUSE_DETAIL_REDIS_EXPIRE_SECOND = 7200
+
+# 房屋列表页面每页数据容量
+HOUSE_LIST_PAGE_CAPACITY = 2
+
+# 房屋列表页面页数缓存时间，单位秒
+HOUES_LIST_PAGE_REDIS_CACHE_EXPIRES = 7200
