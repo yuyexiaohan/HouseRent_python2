40f7656ce13e4dd92e5ade643d7c2a027dea02cb
achjiang <htxz_jiang@163.com>
2019-04-28 00:46:34 +0800

    add detail function

--- ihome/static/js/ihome/detail.js
+++ ihome/static/js/ihome/detail.js
@@ -2,6 +2,7 @@ function hrefBack() {
     history.go(-1);
 }
 
+// 解析提取url中的查询字符串参数
 function decodeQuery(){
     var search = decodeURI(document.location.search);
     return search.replace(/(^\?)/, '').split('&').reduce(function(result, item){
@@ -11,13 +12,30 @@ function decodeQuery(){
     }, {});
 }
 
+// 获取详情页数据
 $(document).ready(function(){
-    var mySwiper = new Swiper ('.swiper-container', {
-        loop: true,
-        autoplay: 2000,
-        autoplayDisableOnInteraction: false,
-        pagination: '.swiper-pagination',
-        paginationType: 'fraction'
-    })
-    $(".book-house").show();
-})
+    // 获取详情页要展示的房间编号
+    var queryData = decodeQuery();
+    var houseId = queryData["id"];
+
+    // 获取房间的详细信息
+    $.get("/api/v1.0/houses/" + houseId, function (resp) {
+        if (resp.errno === "0") {
+            $(".swiper-container").html(template("house-image-tmpl", {img_urls: resp.data.house.img_urls, price: resp.data.house.price}));
+            $(".detail-con").html(template("house-detail-tmpl", {house:resp.data.house}))
+
+            // resp.user_id为访问页面用户,resp.data.user_id为房东
+            if (resp.data.user_id !== resp.data.house.user_id) {
+                $(".book-house").attr("href", "/booking.html?hid=" + resp.data.house.hid);
+                $(".book-house").show();
+            }
+            var mySwiper = new Swiper ('.swiper-container', {
+                loop: true,
+                autoplay: 2000,
+                autoplayDisableOnInteraction: false,
+                pagination: '.swiper-pagination',
+                paginationType: 'fraction'
+            });
+        }
+    });
+});
