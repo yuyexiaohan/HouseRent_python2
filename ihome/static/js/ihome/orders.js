afc75e11dc3f9283691f9e7c71f8cbd182fd8a6b
achjiang <htxz_jiang@163.com>
2019-05-05 23:19:17 +0800

    add ajax request in orders.js

--- ihome/static/js/ihome/orders.js
+++ ihome/static/js/ihome/orders.js
@@ -14,11 +14,76 @@ function getCookie(name) {
     return r ? r[1] : undefined;
 }
 
+// $(document).ready(function(){
+//     $('.modal').on('show.bs.modal', centerModals);      //当模态框出现的时候
+//     $(window).on('resize', centerModals);
+//     $(".order-comment").on("click", function(){
+//         var orderId = $(this).parents("li").attr("order-id");
+//         $(".modal-comment").attr("order-id", orderId);
+//     });
+// });
+
 $(document).ready(function(){
     $('.modal').on('show.bs.modal', centerModals);      //当模态框出现的时候
     $(window).on('resize', centerModals);
-    $(".order-comment").on("click", function(){
-        var orderId = $(this).parents("li").attr("order-id");
-        $(".modal-comment").attr("order-id", orderId);
+    // 查询房客订单
+    $.get("/api/v1.0/user/orders?role=custom", function(resp){
+        if ("0" == resp.errno) {
+            $(".orders-list").html(template("orders-list-tmpl", {orders:resp.data.orders}));
+            $(".order-pay").on("click", function () {
+                var orderId = $(this).parents("li").attr("order-id");
+                $.ajax({
+                    url: "/api/v1.0/orders/" + orderId + "/payment",
+                    type: "post",
+                    dataType: "json",
+                    headers: {
+                        "X-CSRFToken": getCookie("csrf_token"),
+                    },
+                    success: function (resp) {
+                        if ("4101" == resp.errno) {
+                            location.href = "/login.html";
+                        } else if ("0" == resp.errno) {
+                            // 引导用户跳转到支付宝连接
+                            location.href = resp.data.pay_url;
+                        }
+                    }
+                });
+            });
+            $(".order-comment").on("click", function(){
+                var orderId = $(this).parents("li").attr("order-id");
+                $(".modal-comment").attr("order-id", orderId);
+            });
+            $(".modal-comment").on("click", function(){
+                var orderId = $(this).attr("order-id");
+                var comment = $("#comment").val()
+                if (!comment) return;
+                var data = {
+                    order_id:orderId,
+                    comment:comment
+                };
+                // 处理评论
+                $.ajax({
+                    url:"/api/v1.0/orders/"+orderId+"/comment",
+                    type:"PUT",
+                    data:JSON.stringify(data),
+                    contentType:"application/json",
+                    dataType:"json",
+                    headers:{
+                        "X-CSRFTOKEN":getCookie("csrf_token"),
+                    },
+                    success:function (resp) {
+                        if ("4101" == resp.errno) {
+                            location.href = "/login.html";
+                        } else if ("0" == resp.errno) {
+                            $(".orders-list>li[order-id="+ orderId +"]>div.order-content>div.order-text>ul li:eq(4)>span").html("已完成");
+                            $("ul.orders-list>li[order-id="+ orderId +"]>div.order-title>div.order-operate").hide();
+                            $("#comment-modal").modal("hide");
+                        }
+                    }
+                });
+            });
+        }
     });
+
+
 });
