8461b626e30fd3545b37ef1d9deabf3488157045
achjiang <htxz_jiang@163.com>
2019-05-06 18:18:07 +0800

    add pay-blue

--- ihome/api_1_0/__init__.py
+++ ihome/api_1_0/__init__.py
@@ -12,4 +12,4 @@ api = Blueprint("api_1_0", __name__)
 
 # 导入蓝图的视图,建立该蓝图下的视图，运行前都需要导入，否则url无法访问视图函数
 # 这里注意前后顺序，先初始化api，再导入demo，否则反复调用，demo中的api就不能先查找到
-from . import index, verify_code, passport, profile, houses, orders
+from . import index, verify_code, passport, profile, houses, orders, pay
