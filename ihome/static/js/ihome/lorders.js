ce5365a6fc50ae5fefe1070fa4e2c5f698718bae
achjiang <htxz_jiang@163.com>
2019-05-06 08:38:03 +0800

    add lorders.js temple

--- ihome/static/js/ihome/lorders.js
+++ ihome/static/js/ihome/lorders.js
@@ -14,15 +14,88 @@ function getCookie(name) {
     return r ? r[1] : undefined;
 }
 
+/* 静态页面 */
+// $(document).ready(function(){
+//     $('.modal').on('show.bs.modal', centerModals);      //当模态框出现的时候
+//     $(window).on('resize', centerModals);
+//     $(".order-accept").on("click", function(){
+//         var orderId = $(this).parents("li").attr("order-id");
+//         $(".modal-accept").attr("order-id", orderId);
+//     });
+//     $(".order-reject").on("click", function(){
+//         var orderId = $(this).parents("li").attr("order-id");
+//         $(".modal-reject").attr("order-id", orderId);
+//     });
+// });
+
+/* 动态获取数据 */
 $(document).ready(function(){
     $('.modal').on('show.bs.modal', centerModals);      //当模态框出现的时候
     $(window).on('resize', centerModals);
-    $(".order-accept").on("click", function(){
-        var orderId = $(this).parents("li").attr("order-id");
-        $(".modal-accept").attr("order-id", orderId);
-    });
-    $(".order-reject").on("click", function(){
-        var orderId = $(this).parents("li").attr("order-id");
-        $(".modal-reject").attr("order-id", orderId);
+    // 查询房东的订单
+    $.get("/api/v1.0/user/orders?role=landlord", function(resp){
+        if ("0" === resp.errno) {
+            $(".orders-list").html(template("orders-list-tmpl", {orders:resp.data.orders}));
+            $(".order-accept").on("click", function(){
+                var orderId = $(this).parents("li").attr("order-id");
+                $(".modal-accept").attr("order-id", orderId);
+            });
+            // 接单处理
+            $(".modal-accept").on("click", function(){
+                var orderId = $(this).attr("order-id");
+                $.ajax({
+                    url:"/api/v1.0/orders/"+orderId+"/status",
+                    type:"PUT",
+                    data:'{"action":"accept"}',
+                    contentType:"application/json",
+                    dataType:"json",
+                    headers:{
+                        "X-CSRFToken":getCookie("csrf_token"),
+                    },
+                    success:function (resp) {
+                        if ("4101" === resp.errno) {
+                            location.href = "/login.html";
+                        } else if ("0" === resp.errno) {
+                            $(".orders-list>li[order-id="+ orderId +"]>div.order-content>div.order-text>ul li:eq(4)>span").html("已接单");
+                            $("ul.orders-list>li[order-id="+ orderId +"]>div.order-title>div.order-operate").hide();
+                            $("#accept-modal").modal("hide");
+                        }
+                    }
+                })
+            });
+            $(".order-reject").on("click", function(){
+                var orderId = $(this).parents("li").attr("order-id");
+                $(".modal-reject").attr("order-id", orderId);
+            });
+            // 处理拒单
+            $(".modal-reject").on("click", function(){
+                var orderId = $(this).attr("order-id");
+                var reject_reason = $("#reject-reason").val();
+                if (!reject_reason) return;
+                var data = {
+                    action: "reject",
+                    reason:reject_reason
+                };
+                $.ajax({
+                    url:"/api/v1.0/orders/"+orderId+"/status",
+                    type:"PUT",
+                    data:JSON.stringify(data),
+                    contentType:"application/json",
+                    headers: {
+                        "X-CSRFToken":getCookie("csrf_token")
+                    },
+                    dataType:"json",
+                    success:function (resp) {
+                        if ("4101" === resp.errno) {
+                            location.href = "/login.html";
+                        } else if ("0" === resp.errno) {
+                            $(".orders-list>li[order-id="+ orderId +"]>div.order-content>div.order-text>ul li:eq(4)>span").html("已拒单");
+                            $("ul.orders-list>li[order-id="+ orderId +"]>div.order-title>div.order-operate").hide();
+                            $("#reject-modal").modal("hide");
+                        }
+                    }
+                });
+            })
+        }
     });
 });
