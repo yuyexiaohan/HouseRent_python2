c9b1160344a704e067bcfa1fe34960e9cfa40993
achjiang <htxz_jiang@163.com>
2019-05-06 18:19:53 +0800

    add save-order-payment-result function

--- ihome/api_1_0/pay.py
+++ ihome/api_1_0/pay.py
@@ -53,3 +53,40 @@ def order_pay(order_id):
     # 构建让用户跳转的支付连接地址
     pay_url = constants.ALIPAY_URL_PREFIX + order_string
     return jsonify(errno=RET.OK, errmsg="OK", data={"pay_url": pay_url})
+
+
+@api.route("/order/payment", methods=["PUT"])
+def save_order_payment_result():
+    """保存订单支付结果"""
+    alipay_dict = request.form.to_dict()
+
+    # 对支付宝的数据进行分离  提取出支付宝的签名参数sign 和剩下的其他数据
+    alipay_sign = alipay_dict.pop("sign")
+
+    # 创建支付宝sdk的工具对象
+    alipay_client = AliPay(
+        appid="2016081600258081",
+        app_notify_url=None,  # 默认回调url
+        app_private_key_path=os.path.join(os.path.dirname(__file__), "keys/app_private_key.pem"),  # 私钥
+        alipay_public_key_path=os.path.join(os.path.dirname(__file__), "keys/alipay_public_key.pem"),
+        # 支付宝的公钥，验证支付宝回传消息使用，不是你自己的公钥,
+        sign_type="RSA2",  # RSA 或者 RSA2
+        debug=True  # 默认False
+    )
+
+    # 借助工具验证参数的合法性
+    # 如果确定参数是支付宝的，返回True，否则返回false
+    result = alipay_client.verify(alipay_dict, alipay_sign)
+
+    if result:
+        # 修改数据库的订单状态信息
+        order_id = alipay_dict.get("out_trade_no")
+        trade_no = alipay_dict.get("trade_no")  # 支付宝的交易号
+        try:
+            Order.query.filter_by(id=order_id).update({"status": "WAIT_COMMENT", "trade_no": trade_no})
+            db.session.commit()
+        except Exception as e:
+            current_app.logger.error(e)
+            db.session.rollback()
+
+    return jsonify(errno=RET.OK, errmsg="OK")
